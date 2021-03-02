b23d6e3fd1237043f451bf91dd5e29ed3368a318
clockworkengineer <robert_tizzard@hotmail.com>
2021-03-02 14:05:05 +0000

    More tests for stringify

--- XML/tests/XML_tests_stringify.cpp
+++ XML/tests/XML_tests_stringify.cpp
@@ -249,57 +249,55 @@ TEST_CASE("Stringify XML with character entities/reference.", "[XML][Stringify][
         REQUIRE(xml.stringify(xNodeRoot) == xmlString);
     }
 
-//       SECTION("Parse entity &quot; in contents area", "[XML][Parse][Entities]")
-//   {
-//     xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-//                 " <root> &quot; </root>\n";
-//     BufferSource xmlSource(xmlString);
-//     std::unique_ptr<XNode> xNodeRoot=xml.parse(xmlSource);
-//     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).getContents() == " \" ");
-//   }
-//   SECTION("Parse entities &apos; &lt; &gt; in contents area", "[XML][Parse][Entities]")
-//   {
-//     xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-//                 " <root> &apos; &lt; &gt; </root>\n";
-//     BufferSource xmlSource(xmlString);
-//     std::unique_ptr<XNode> xNodeRoot=xml.parse(xmlSource);
-//     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).getContents() == " ' < > ");
-//   }
-//   SECTION("Parse reference &#x00A5; in contents area", "[XML][Parse][Entities]")
-//   {
-//     xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-//                 " <root> &#x00A5; </root>\n";
-//     BufferSource xmlSource(xmlString);
-//     std::unique_ptr<XNode> xNodeRoot=xml.parse(xmlSource);
-//     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).getContents() == " ¥ ");
-//   }
-//   SECTION("Parse reference &#163; in contents area", "[XML][Parse][Entities]")
-//   {
-//     xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?> "
-//                 "<root> &#163; </root>\n";
-//     BufferSource xmlSource(xmlString);
-//     std::unique_ptr<XNode> xNodeRoot=xml.parse(xmlSource);
-//     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[0]).getContents() == " £ ");
-//   }
-//   SECTION("Parse entity &amp;&quot;&apos;&gt;&lt; in attribute value", "[XML][Parse][Entities]")
-//   {
-//     xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-//                 " <root attr1=\" &amp;&quot;&apos;&gt;&lt; \">\n"
-//                 "</root>\n";
-//     BufferSource xmlSource(xmlString);
-//     std::unique_ptr<XNode> xNodeRoot=xml.parse(xmlSource);
-//     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).attributes.size() == 1);
-//     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).attributes[0].name == "attr1");
-//     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).attributes[0].value == " &\"'>< ");
-//   }
-//   SECTION("Parse references &#x00A5;&#163 in attribute value", "[XML][Parse][Entities]")
-//   {
-//     xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-//                 " <root attr1=\" &#x00A5;&#163; \"></root>\n";
-//     BufferSource xmlSource(xmlString);
-//     std::unique_ptr<XNode> xNodeRoot=xml.parse(xmlSource);
-//     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).attributes.size() == 1);
-//     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).attributes[0].name == "attr1");
-//     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).attributes[0].value == " ¥£ ");
-//   }
+    SECTION("Stringify entity &quot; in contents area", "[XML][Stringify][Entities]")
+    {
+        xmlString = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                    "<root> &quot; </root>\n";
+        BufferSource xmlSource(xmlString);
+        std::unique_ptr<XNode> xNodeRoot = xml.parse(xmlSource);
+        REQUIRE(xml.stringify(xNodeRoot) == xmlString);
+    }
+      SECTION("Stringify entities &apos; &lt; &gt; in contents area", "[XML][Stringify][Entities]")
+      {
+        xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                    "<root> &apos; &lt; &gt; </root>\n";
+        BufferSource xmlSource(xmlString);
+        std::unique_ptr<XNode> xNodeRoot = xml.parse(xmlSource);
+        REQUIRE(xml.stringify(xNodeRoot) == xmlString);
+      }
+      SECTION("Stringify character reference &#x00A5; in contents area", "[XML][Stringify][Entities]")
+      {
+        xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                    "<root> &#x00A5; </root>\n";
+        BufferSource xmlSource(xmlString);
+        std::unique_ptr<XNode> xNodeRoot = xml.parse(xmlSource);
+        REQUIRE(xml.stringify(xNodeRoot) == xmlString);
+      }
+      SECTION("Stringify character reference &#163; in contents area", "[XML][Stringify][Entities]")
+      {
+        xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                    "<root> &#163; </root>\n";
+        BufferSource xmlSource(xmlString);
+        std::unique_ptr<XNode> xNodeRoot = xml.parse(xmlSource);
+        REQUIRE(xml.stringify(xNodeRoot) == xmlString);
+      }
+    //   SECTION("Stringify entities &amp;&quot;&apos;&gt;&lt; in attribute value", "[XML][Stringify][Entities]")
+    //   {
+    //     xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+    //                 "<root attr1=\" &amp;&quot;&apos;&gt;&lt; \">\n"
+    //                 "</root>\n";
+    //     BufferSource xmlSource(xmlString);
+    //     std::unique_ptr<XNode> xNodeRoot = xml.parse(xmlSource);
+    //     REQUIRE(xml.stringify(xNodeRoot) == xmlString);
+    //   }
+    //   SECTION("Parse references &#x00A5;&#163 in attribute value", "[XML][Parse][Entities]")
+    //   {
+    //     xmlString="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+    //                 " <root attr1=\" &#x00A5;&#163; \"></root>\n";
+    //     BufferSource xmlSource(xmlString);
+    //     std::unique_ptr<XNode> xNodeRoot=xml.parse(xmlSource);
+    //     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).attributes.size() == 1);
+    //     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).attributes[0].name == "attr1");
+    //     REQUIRE(XNodeRef<XNodeElement>((*xNodeRoot)[1]).attributes[0].value == " ¥£ ");
+    //   }
 }
