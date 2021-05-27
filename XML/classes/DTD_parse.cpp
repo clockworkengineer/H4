028364e543a6b6f359d338b6e264cc3987553e00
clockworkengineer <robert_tizzard@hotmail.com>
2021-05-27 07:03:30 +0100

    Refactoring DTD.

--- XML/classes/DTD_parse.cpp
+++ XML/classes/DTD_parse.cpp
@@ -107,27 +107,23 @@ namespace H4
     {
         while (dtdSource.more())
         {
-            std::string translated;
             if (dtdSource.match(U"<!ENTITY"))
             {
                 extractTagBody(dtdSource);
             }
             else if (dtdSource.match(U"<!ELEMENT"))
             {
-                translated = parseTranslateParameterEntities(extractTagBody(dtdSource));
-                BufferSource dtdTranslatedSource(translated);
+                BufferSource dtdTranslatedSource(parseTranslateParameterEntities(extractTagBody(dtdSource)));
                 parseElement(dtdTranslatedSource);
             }
             else if (dtdSource.match(U"<!ATTLIST"))
             {
-                translated = parseTranslateParameterEntities(extractTagBody(dtdSource));
-                BufferSource dtdTranslatedSource(translated);
+                BufferSource dtdTranslatedSource(parseTranslateParameterEntities(extractTagBody(dtdSource)));
                 parseAttributeList(dtdTranslatedSource);
             }
             else if (dtdSource.match(U"<!NOTATION"))
             {
-                translated = parseTranslateParameterEntities(extractTagBody(dtdSource));
-                BufferSource dtdTranslatedSource(translated);
+                BufferSource dtdTranslatedSource(parseTranslateParameterEntities(extractTagBody(dtdSource)));
                 parseNotation(dtdTranslatedSource);
             }
             else if (dtdSource.match(U"<!--"))
@@ -254,240 +250,6 @@ namespace H4
     /// </summary>
     /// <param name=""></param>
     /// <returns></returns>
-    bool DTD::parseIsChoiceOrSequence(ISource &contentSpecSource)
-    {
-        bool choice = false;
-        long start = contentSpecSource.position();
-        while (contentSpecSource.more() &&
-               contentSpecSource.current() != '|' &&
-               contentSpecSource.current() != ',')
-        {
-            contentSpecSource.next();
-        }
-        if (contentSpecSource.more() && contentSpecSource.current() == '|')
-        {
-            choice = true;
-        }
-        contentSpecSource.backup(contentSpecSource.position() - start);
-        return (choice);
-    }
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name=""></param>
-    /// <returns></returns>
-    void DTD::parseElementCP(ISource &contentSpecSource, IDestination &contentSpecDestination)
-    {
-        contentSpecSource.next();
-        contentSpecSource.ignoreWS();
-        if (validNameStartChar(contentSpecSource.current()))
-        {
-            parseElementName(contentSpecSource, contentSpecDestination);
-        }
-        else if (parseIsChoiceOrSequence(contentSpecSource))
-        {
-            parseElementChoice(contentSpecSource, contentSpecDestination);
-        }
-        else
-        {
-            parseElementSequence(contentSpecSource, contentSpecDestination);
-        }
-        if (contentSpecSource.current() == '*' ||
-            contentSpecSource.current() == '+' ||
-            contentSpecSource.current() == '?')
-        {
-            contentSpecDestination.add(contentSpecSource.current());
-            contentSpecSource.next();
-            contentSpecSource.ignoreWS();
-        }
-    }
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name=""></param>
-    /// <returns></returns>
-    void DTD::parseElementChoice(ISource &contentSpecSource, IDestination &contentSpecDestination)
-    {
-        if (contentSpecSource.current() != '(')
-        {
-            throw XML::SyntaxError("Invalid content region specification.");
-        }
-        contentSpecDestination.add("(");
-        parseElementCP(contentSpecSource, contentSpecDestination);
-        while (contentSpecSource.more() && contentSpecSource.current() == '|')
-        {
-            contentSpecDestination.add("|");
-            parseElementCP(contentSpecSource, contentSpecDestination);
-        }
-        if (contentSpecSource.current() != ')')
-        {
-            throw XML::SyntaxError("Invalid content region specification.");
-        }
-        contentSpecDestination.add(")");
-        contentSpecSource.next();
-        contentSpecSource.ignoreWS();
-    }
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name=""></param>
-    /// <returns></returns>
-    void DTD::parseElementSequence(ISource &contentSpecSource, IDestination &contentSpecDestination)
-    {
-        if (contentSpecSource.current() != '(')
-        {
-            throw XML::SyntaxError("Invalid content region specification.");
-        }
-        contentSpecDestination.add("(");
-        parseElementCP(contentSpecSource, contentSpecDestination);
-        while (contentSpecSource.more() && contentSpecSource.current() == ',')
-        {
-            parseElementCP(contentSpecSource, contentSpecDestination);
-        }
-        if (contentSpecSource.current() != ')')
-        {
-            throw XML::SyntaxError("Invalid content region specification.");
-        }
-        contentSpecDestination.add(")");
-        contentSpecSource.next();
-        contentSpecSource.ignoreWS();
-    }
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name=""></param>
-    /// <returns></returns>
-    void DTD::parseElementName(ISource &contentSpecSource, IDestination &contentSpecDestination)
-    {
-        contentSpecDestination.add("(<");
-        contentSpecDestination.add(contentSpecSource.current());
-        contentSpecSource.next();
-        while (contentSpecSource.more() && validNameChar(contentSpecSource.current()))
-        {
-            contentSpecDestination.add(contentSpecSource.current());
-            contentSpecSource.next();
-        }
-        contentSpecDestination.add(">)");
-        contentSpecSource.ignoreWS();
-    }
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name=""></param>
-    /// <returns></returns>
-    void DTD::parseElementChildren(ISource &contentSpecSource, IDestination &contentSpecDestination)
-    {
-        if (contentSpecSource.current() == '(')
-        {
-            if (parseIsChoiceOrSequence(contentSpecSource))
-            {
-                parseElementChoice(contentSpecSource, contentSpecDestination);
-            }
-            else
-            {
-                parseElementSequence(contentSpecSource, contentSpecDestination);
-            }
-            if (contentSpecSource.current() == '*' ||
-                contentSpecSource.current() == '+' ||
-                contentSpecSource.current() == '?')
-            {
-                contentSpecDestination.add(contentSpecSource.current());
-                contentSpecSource.next();
-                contentSpecSource.ignoreWS();
-            }
-        }
-        else
-        {
-            throw XML::SyntaxError("Invalid content region specification.");
-        }
-    }
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name=""></param>
-    /// <returns></returns>
-    void DTD::parseElementMixedContent(ISource &contentSpecSource, IDestination &contentSpecDestination)
-    {
-        contentSpecSource.ignoreWS();
-        contentSpecDestination.add("((<#PCDATA>)");
-        if (contentSpecSource.current() == '|')
-        {
-            while (contentSpecSource.more() && contentSpecSource.current() == '|')
-            {
-                contentSpecDestination.add("|");
-                contentSpecSource.next();
-                contentSpecSource.ignoreWS();
-                if (validNameStartChar(contentSpecSource.current()))
-                {
-                    parseElementName(contentSpecSource, contentSpecDestination);
-                }
-                else
-                {
-                    throw XML::SyntaxError("Invalid content region specification.");
-                }
-            }
-            if (contentSpecSource.current() != ')')
-            {
-                throw XML::SyntaxError("Invalid content region specification.");
-            }
-            contentSpecDestination.add(")");
-            contentSpecSource.next();
-            contentSpecSource.ignoreWS();
-            if (contentSpecSource.current() == '*')
-            {
-                contentSpecDestination.add("*");
-                contentSpecDestination.add(contentSpecSource.current());
-                contentSpecSource.next();
-            }
-            if (contentSpecSource.more() && !std::iswspace(contentSpecSource.current()))
-            {
-                throw XML::SyntaxError("Invalid content region specification.");
-            }
-        }
-        else if (contentSpecSource.current() == ')')
-        {
-            contentSpecDestination.add(")");
-        }
-        else
-        {
-            throw XML::SyntaxError("Invalid content region specification.");
-        }
-    }
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name=""></param>
-    /// <returns></returns>
-    void DTD::parseElementContentSpecification(XMLValue &contentSpec)
-    {
-        BufferSource contentSpecSource(contentSpec.unparsed);
-        BufferDestination contentSpecDestination;
-        contentSpecSource.ignoreWS();
-        if (contentSpecSource.current() == '(')
-        {
-            contentSpecSource.next();
-            contentSpecSource.ignoreWS();
-            if (contentSpecSource.match(U"#PCDATA"))
-            {
-                parseElementMixedContent(contentSpecSource, contentSpecDestination);
-            }
-            else
-            {
-                contentSpecSource.backup(contentSpecSource.position());
-                parseElementChildren(contentSpecSource, contentSpecDestination);
-            }
-        }
-        else
-        {
-            throw XML::SyntaxError("Invalid content region specification.");
-        }
-        contentSpec.parsed = contentSpecDestination.getBuffer();
-    }
-    /// <summary>
-    ///
-    /// </summary>
-    /// <param name=""></param>
-    /// <returns></returns>
     void DTD::parsePostProcessing()
     {
         for (auto &element : m_elements)
@@ -560,8 +322,8 @@ namespace H4
     {
         if (m_external.type == "SYSTEM")
         {
-            FileSource dtdFile(m_external.systemID);
-            parseParameterENTITIES(dtdFile);
+            FileSource dtdFilePass1(m_external.systemID);
+            parseParameterENTITIES(dtdFilePass1);
             FileSource dtdFilePass2(m_external.systemID);
             parseTranslateParameterENTITIES(dtdFilePass2);
         }
@@ -754,7 +516,7 @@ namespace H4
         dtdSource.ignoreWS();
     }
     /// <summary>
-    /// Parse m_externally defined DTD.
+    /// Parse externally defined DTD.
     /// </summary>
     /// <param name="dtdSource">DTD source stream.</param>
     /// <returns></returns>
